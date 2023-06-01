eea6ff173f49dd6e6f50b872a804312c53bf8fce
Viktoryia Stoliar <vikynyshka88genya@gmail.com>
2023-06-01 20:12:42 +0300

    server

---
+++ src/controller/user.controller.js
@@ -0,0 +1,46 @@
+const route = require('express').Router()
+const { getAllUsers, getUserById, createUsers, updateUsers } = require('../service/user.service')
+
+route.get('/', async (req, res) => {
+    try {
+        const data = await getAllUsers()
+        res.status(200).send(data)
+    } catch (error) {
+        res.status(404).send(error.message)
+    }
+})
+
+route.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const data = await getUserById(id)
+        res.status(200).send(data)
+    } catch (error) {
+        res.status(404).send(error.message)
+    }
+})
+
+route.post('/', async (req, res) => {
+    try {
+        const { name, surname, email, pwd } = req.body
+        const data = await createUsers(name, surname, email, pwd)
+        res.status(200).send(data)
+    } catch (error) {
+        res.status(404).send(error.message)
+    }
+})
+
+route.put('/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const { name, surname, email, pwd } = req.body
+        const data = await updateUsers(id, name, surname, email, pwd)
+        res.status(200).send(data)
+    } catch (error) {
+        res.status(404).send(error.message)
+    }
+})
+
+
+
+module.exports = route
---
+++ src/service/user.service.js
@@ -0,0 +1,27 @@
+const { getAllUsersDB, getUserByIdDB, createUsersDB, updateUsersDB } = require('../repository/user.repository')
+
+async function getAllUsers() {
+    const data = await getAllUsersDB()
+    if (!data.length) throw new Error('no data')
+    return data
+}
+
+async function getUserById(id) {
+    const data = await getUserByIdDB(id)
+    if (!data.length) throw new Error('id not found')
+    return data
+}
+
+async function createUsers(name, surname, email, pwd) {
+    const data = await createUsersDB(name, surname, email, pwd)
+    if (!data.length) throw new Error('no data')
+    return data
+}
+
+async function updateUsers(id, name, surname, email, pwd) {
+    const data = await updateUsersDB(id, name, surname, email, pwd)
+    if (!data.length) throw new Error('no data')
+    return data
+}
+
+module.exports = { getAllUsers, getUserById, createUsers, updateUsers }
