4c97a3c6ae5c20c88233e9ee0ce3e56045b5609c
Viktoryia Stoliar <vikynyshka88genya@gmail.com>
2023-06-12 23:03:15 +0300

    TaskManager

--- src/service/api.service.js
+++ src/service/api.service.js
@@ -4,22 +4,22 @@ const bcrypt = require('bcrypt');
 const saltround = 4;
 
 async function createUser(name, surname, email, pwd) {
-    const foundUser = await getUserByEmailDB(email)
-    if (foundUser.length) throw new Error('email already exist')
+  const foundUser = await getUserByEmailDB(email);
+  if (foundUser.length) throw new Error('email already exist');
 
-    const hashedPassword = await bcrypt.hash(pwd, saltround)
+  const hashedPassword = await bcrypt.hash(pwd, saltround);
 
-    const data = await createUserDB(name, surname, email, hashedPassword)
-    if (!data.length) throw new Error('Data is not found')
-    return data
+  const data = await createUserDB(name, surname, email, hashedPassword);
+  if (!data.length) throw new Error('Data is not found');
+  return data;
 }
 
 async function authorisationUser(email, pwd) {
-    const findUser = await getUserByEmailDB(email)
-    if (!findUser.length) throw new Error('email doesn`t exist')
-    const bool = await bcrypt.compare(pwd, findUser[0].pwd)
-    if (!bool) throw new Error ('pwd not match!')
-    return findUser
+  const findUser = await getUserByEmailDB(email);
+  if (!findUser.length) throw new Error('email doesn`t exist');
+  const bool = await bcrypt.compare(pwd, findUser[0].pwd);
+  if (!bool) throw new Error('pwd not match!');
+  return findUser;
 }
 
-module.exports = { createUser, authorisationUser }
+module.exports = { createUser, authorisationUser };
